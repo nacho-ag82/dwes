c22671a829d1745f63e77132f5db28e0a1bb0885
nacho-ag82 <iagugar398@g.educaand.es>
2024-12-10 13:00:17 +0100

    try

--- bd/accesoABD.php
+++ bd/accesoABD.php
@@ -1,6 +1,12 @@
 <?php 
-$mysql = "mysql:host=localhost;dbname=dwes_manana_prueba;charset=UTF8";
-$user = "dwes_manana";
-$password = "root";
-$conexion = new PDO($mysql, $user, $password);
+try {
+    $mysql = "mysql:host=localhost;dbname=dwes_manana_prueba;charset=UTF8";
+    $user = "dwes_manana";
+    $password = "root";
+    $conexion = new PDO($mysql, $user, $password);
+    # code...
+} catch (\Throwable $e) {
+    # code...
+}
+
 ?>
